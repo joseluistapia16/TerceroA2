6472fa2de812aa49cb14fa30e9695c88e6269b77
user <user@MAESTRO>
2024-11-25 13:56:49 -0500

    Conexion a MySql (Correccion libreria) 25/11/2024 139:57 pM

--- src/dao/Conexion.java
+++ src/dao/Conexion.java
@@ -1,21 +1,26 @@
-
 package dao;
-import com.sun.jdi.connect.spi.Connection;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Conexion {
-   private Connection conect = null;
-   public Connection conectar(String base){
-       try{
-       Class.forName("com.mysql.cj.jdbc.Driver");
-       String ruta = "jdbc:mysql://localhost:3307/" + base;
-       conect = (Connection) DriverManager.getConnection(ruta, "root", "");
-       }
-       catch(Exception e){
-           System.out.println("Error de Conexion.");   
-       }
-       return conect;
-   }
+
+    private Connection conect = null;
+
+ public Connection conectar(String base) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/" + base;
+            conect = (Connection) DriverManager.getConnection(url, "root", "");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conect;
+    }
+
     public static void main(String[] args) {
         Conexion obp = new Conexion();
         System.out.println(obp.conectar("terceroa"));
